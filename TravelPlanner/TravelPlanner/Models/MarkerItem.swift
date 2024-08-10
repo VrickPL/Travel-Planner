d2fa50e63fce5bda270a3a899aa446de1c3c88d7
Jan Kazubski <jkazubski@icloud.com>
2024-08-10 13:54:32 +0200

    Added missing MarkerItem class due to mistake in previous commit

--- TravelPlanner/TravelPlanner/Models/MarkerItem.swift
+++ TravelPlanner/TravelPlanner/Models/MarkerItem.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+import MapKit
+import SwiftData
+
+@Model
+class MarkerItem: Identifiable {
+    var id: String
+    var placeName: String
+    var placeDescription: String
+    var longitude: Double
+    var latitude: Double
+
+    init(
+        placeName: String,
+        placeDescription: String,
+        longitude: Double,
+        latitude: Double
+    ) {
+        self.id = UUID().uuidString
+        self.placeName = placeName
+        self.placeDescription = placeDescription
+        self.longitude = longitude
+        self.latitude = latitude
+    }
+
+    func asMKMapItem() -> MKMapItem {
+        let placemark = MKPlacemark(
+            coordinate: CLLocationCoordinate2D(
+                latitude: latitude,
+                longitude: longitude
+            )
+        )
+
+        let mapItem = MKMapItem(placemark: placemark)
+
+        mapItem.name = placeName
+        return mapItem
+    }
+}
